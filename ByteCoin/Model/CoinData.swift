66fe7b15aeb027d332ca22de36ec283001a42c3a
Islam Nurmukhametov <islam.nurmuhametov@omega-r.com>
2023-08-21 15:58:55 +0300

    completed challenge

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Ислам Нурмухаметов on 21.08.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
